f91ce758171fff1e349e69badaa0fcdf81a5b2a9
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-06-02 00:53:43 +0600

    skill

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../../components/About/About";
 import Intro from "../../components/Intro/Intro";
+import Skills from "../../components/Skills/Skills";
 
 export default function Home() {
    return (
       <>
          <Intro />
          <About />
+         <Skills />
       </>
    );
 }
