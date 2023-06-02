643a99f2fcc84c84090ac04d8fd09f9818c29659
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-06-02 17:05:10 +0600

    display project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import SingleProject from "./SingleProject";
 
 export default function Projects() {
+   const [projects, setProjects] = useState();
+   useEffect(() => {
+      fetch("http://localhost:5000/projects/?limit=3")
+         .then((res) => res.json())
+         .then((data) => setProjects(data))
+         .catch((e) => console.log(e));
+   }, []);
    return (
       <div className="container">
          <SectionTitle title="Most Recent Projects" color="black" />
 
-         <div className="flex gap-5">
-            <SingleProject />
+         <div className="grid grid-cols-3 gap-5">
+            {projects?.map((project) => (
+               <SingleProject key={project._id} data={project} />
+            ))}
          </div>
       </div>
    );
--- src/components/Projects/SingleProject.jsx
+++ src/components/Projects/SingleProject.jsx
@@ -1,38 +1,28 @@
 import { FaGithub, FaGlobe } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-export default function SingleProject() {
+export default function SingleProject({ data }) {
+   const { title, photo, description, tags, date, github, liveSite } = data;
    return (
-      <div className="card w-1/3 bg-base-100 shadow-xl">
+      <div className="card bg-base-100 shadow-xl">
          <figure>
-            <img
-               src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-               alt="Shoes"
-            />
+            <img src={photo} alt="Shoes" />
          </figure>
          <div className="card-body">
-            <h2 className="card-title">Landing Page</h2>
-            <div className="badge badge-secondary">10-02-2022</div>
-            <p className="my-5">
-               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-               Similique dolorum asperiores suscipit sit ex, tempora sunt quas
-               inventore odio hic enim, ipsam, repellendus autem fuga aspernatur
-               ipsa veniam distinctio magni!
-            </p>
+            <h2 className="card-title">{title}</h2>
+            <div className="badge badge-secondary">{date}</div>
+            <p className="my-5">{description}</p>
             <div className="card-actions justify-start">
                <div className="badge badge-outline hover:text-blue-500 cursor-default">
-                  React
-               </div>
-               <div className="badge badge-outline hover:text-blue-500 cursor-default">
-                  Tailwind
+                  {tags}
                </div>
             </div>
 
             <div className="flex gap-3 mt-5">
-               <Link to="" className="btn flex gap-2 flex-1">
+               <Link to={github} className="btn flex gap-2 flex-1">
                   <FaGlobe /> View Live
                </Link>
-               <Link to="" className="btn flex gap-2 flex-1">
+               <Link to={liveSite} className="btn flex gap-2 flex-1">
                   <FaGithub /> GitHub
                </Link>
             </div>
