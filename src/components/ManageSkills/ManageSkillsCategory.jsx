f79ef74e56bc8377661f0d6b836e71146bd7dacc
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-06-23 10:10:22 +0600

    skill category management

--- src/components/ManageSkills/ManageSkillsCategory.jsx
+++ src/components/ManageSkills/ManageSkillsCategory.jsx
@@ -4,6 +4,9 @@ import {
    AiOutlineQuestionCircle,
    AiOutlinePoweroff,
 } from "react-icons/ai";
+import SkillsCategoryItem from "./SkillsCategoryItem";
+import toastr from "toastr";
+import { useState } from "react";
 
 export default function ManageSkillsCategory() {
    const {
@@ -11,91 +14,166 @@ export default function ManageSkillsCategory() {
       handleSubmit,
       formState: { errors },
    } = useForm();
+
+   const [categories, setCategories] = useState([]);
+
+   const fetchCategory = () => {
+      fetch("https://ashfatul-islam-server.vercel.app/skills-category")
+         .then((res) => res.json())
+         .then((res) => {
+            setCategories(res);
+         })
+         .catch((e) => console.log(e));
+   };
+
+   fetchCategory();
+
+   const deleteCategory = (id) => {
+      fetch(
+         `https://ashfatul-islam-server.vercel.app/skills-category/delete/${id}`,
+         {
+            method: "DELETE",
+         }
+      )
+         .then((res) => res.json())
+         .then(() => {
+            toastr.info("Category Deleted Successfully");
+            fetchCategory();
+         })
+         .catch((e) => console.log(e));
+   };
+
    const updateSkillsCategory = (data) => {
-      console.log(data);
+      fetch("https://ashfatul-islam-server.vercel.app/skills-category", {
+         method: "POST",
+         headers: {
+            "content-type": "application/json",
+         },
+         body: JSON.stringify(data),
+      })
+         .then((res) => res.json())
+         .then(() => {
+            toastr.success("Skill Category Added Successfully");
+            fetchCategory();
+         })
+         .catch((e) => console.log(e));
    };
    return (
-      <div className="form-container mt-5 flex-1">
-         <form
-            onSubmit={handleSubmit((data) => updateSkillsCategory(data))}
-            className="bg-white rounded-lg p-5 w-full"
-         >
-            <h2 className="text-2xl text-center mb-5">Skills Category!</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+         <div className="table-container w-full md:w-3/5">
+            <table className="w-full table">
+               <thead>
+                  <tr>
+                     <th>Order</th>
+                     <th>Name</th>
+                     <th>Status</th>
+                     <th>Action</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {categories.length > 0 ? (
+                     categories.map((item) => (
+                        <SkillsCategoryItem
+                           key={item._id}
+                           skill={item}
+                           handleDelete={deleteCategory}
+                        />
+                     ))
+                  ) : (
+                     <tr>
+                        <td className="text-center" colSpan={4}>
+                           No Skill Category Found
+                        </td>
+                     </tr>
+                  )}
+               </tbody>
+            </table>
+         </div>
 
-            <div className="form-control w-full">
-               <label className="label">
-                  <span className="label-text">Order</span>
-               </label>
-               <label className="input-group">
-                  <span>
-                     <AiOutlineFieldNumber />
-                  </span>
-                  <input
-                     type="number"
-                     placeholder={"0"}
-                     defaultValue={""}
-                     className="input input-bordered w-full"
-                     {...register("order", { required: true })}
-                  />
-               </label>
-            </div>
+         <div className="form-container w-full md:w-2/5">
+            <form
+               onSubmit={handleSubmit((data) => updateSkillsCategory(data))}
+               className="bg-white rounded-lg p-5 w-full"
+            >
+               <h2 className="text-2xl text-center mb-5">Skills Category!</h2>
 
-            {errors.order && (
-               <span className="text-red-500">This field is required</span>
-            )}
+               <div className="form-control w-full">
+                  <label className="label">
+                     <span className="label-text">Order</span>
+                  </label>
+                  <label className="input-group">
+                     <span>
+                        <AiOutlineFieldNumber />
+                     </span>
+                     <input
+                        type="number"
+                        placeholder={"0"}
+                        defaultValue={""}
+                        className="input input-bordered w-full"
+                        {...register("order", {
+                           required: true,
+                           valueAsNumber: true,
+                        })}
+                     />
+                  </label>
+               </div>
 
-            <div className="form-control w-full">
-               <label className="label">
-                  <span className="label-text">Category Name</span>
-               </label>
-               <label className="input-group">
-                  <span>
-                     <AiOutlineQuestionCircle />
-                  </span>
-                  <input
-                     type="text"
-                     placeholder={"Frontend"}
-                     defaultValue={""}
-                     className="input input-bordered w-full"
-                     {...register("categoryName", { required: true })}
-                  />
-               </label>
-            </div>
+               {errors.order && (
+                  <span className="text-red-500">This field is required</span>
+               )}
 
-            {errors.categoryName && (
-               <span className="text-red-500">This field is required</span>
-            )}
+               <div className="form-control w-full">
+                  <label className="label">
+                     <span className="label-text">Category Name</span>
+                  </label>
+                  <label className="input-group">
+                     <span>
+                        <AiOutlineQuestionCircle />
+                     </span>
+                     <input
+                        type="text"
+                        placeholder={"Frontend"}
+                        defaultValue={""}
+                        className="input input-bordered w-full"
+                        {...register("categoryName", { required: true })}
+                     />
+                  </label>
+               </div>
 
-            <div className="form-control w-full">
-               <label className="label">
-                  <span className="label-text">Status</span>
-               </label>
-               <label className="input-group">
-                  <span>
-                     <AiOutlinePoweroff />
-                  </span>
-                  <select
-                     type="text"
-                     placeholder={"photo.png"}
-                     defaultValue={""}
-                     className="input input-bordered w-full"
-                     {...register("status", { required: true })}
-                  >
-                     <option value="active">Active</option>
-                     <option value="disabled">Disabled</option>
-                  </select>
-               </label>
-            </div>
-            {errors.status && (
-               <span className="text-red-500">This field is required</span>
-            )}
+               {errors.categoryName && (
+                  <span className="text-red-500">This field is required</span>
+               )}
 
-            <input
-               type="submit"
-               className="btn mt-10 block w-full"
-               value={"Add Skills Category"}
-            />
-         </form>
+               <div className="form-control w-full">
+                  <label className="label">
+                     <span className="label-text">Status</span>
+                  </label>
+                  <label className="input-group">
+                     <span>
+                        <AiOutlinePoweroff />
+                     </span>
+                     <select
+                        type="text"
+                        placeholder={"photo.png"}
+                        className="input input-bordered w-full"
+                        {...register("status", { required: true })}
+                     >
+                        <option value="active">Active</option>
+                        <option value="disabled">Disabled</option>
+                     </select>
+                  </label>
+               </div>
+               {errors.status && (
+                  <span className="text-red-500">This field is required</span>
+               )}
+
+               <input
+                  type="submit"
+                  className="btn mt-10 block w-full"
+                  value={"Add Skills Category"}
+               />
+            </form>
+         </div>
       </div>
    );
 }
---
+++ src/components/ManageSkills/SkillsCategoryItem.jsx
@@ -0,0 +1,19 @@
+import { FaTrash } from "react-icons/fa";
+
+export default function SkillsCategoryItem({ skill, handleDelete }) {
+   return (
+      <tr>
+         <td>{skill.order}</td>
+         <td>{skill.categoryName}</td>
+         <td>{skill.status}</td>
+         <td>
+            <button
+               className="btn btn-primary btn-sm"
+               onClick={() => handleDelete(skill._id)}
+            >
+               <FaTrash />
+            </button>
+         </td>
+      </tr>
+   );
+}
