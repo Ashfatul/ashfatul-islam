d6e77f359bbfcfb0fe0c6b3a270b757330e52be9
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-06-24 19:42:44 +0600

    update

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,23 +1,34 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import SingleCategory from "./SingleCategory";
+import Loader from "../Loader/Loader";
 
 export default function Skills() {
    const [category, setCategory] = useState([]);
+   const [loading, setLoading] = useState(false);
    useEffect(() => {
+      setLoading(true);
       fetch("https://ashfatul-islam-server.vercel.app/skills-category")
          .then((res) => res.json())
-         .then((data) => setCategory(data))
-         .catch((e) => console.log(e));
+         .then((data) => {
+            setCategory(data);
+            setLoading(false);
+         })
+         .catch((e) => {
+            console.log(e);
+            setLoading(false);
+         });
    }, []);
 
    return (
       <div className="container" id="skills">
          <SectionTitle title="Skills Set" color="black" />
 
-         {category.map((c) => (
-            <SingleCategory key={c._id} data={c} />
-         ))}
+         {loading ? (
+            <Loader />
+         ) : (
+            category.map((c) => <SingleCategory key={c._id} data={c} />)
+         )}
       </div>
    );
 }
